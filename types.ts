649b18bba206653a9f13e02a394ba365c954b768
Jack Schneble <schneble.jack@gmail.com>
2022-08-24 21:32:55 -0400

    types file from old branch

---
+++ types.ts
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+import { IconType } from "react-icons";
+
+export interface IProject {
+    name: string;
+    description: string;
+    image_path: string;
+    deployed_url: string;
+   source_url: string;
+   
+ 
+  }
+  
