015733601dca565ddac6ad4e5f68adacffd9e5ad
Jack Schneble <schneble.jack@gmail.com>
2022-09-26 21:17:53 -0400

    col size

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -94,7 +94,7 @@ export default function Container({
    
           <nav className=' sticky w-full  z-[1] top-2 md:top-4 max-w-2xl  rounded-md mx-auto flex justify-between items-center pad bg-stickyy dark:bg-transparent '>
           <div className='sticky w-full top-2 md:top-4 max-w-2xl px-4 py-2 rounded-md mx-auto flex justify-between items-center   bg-transparent  z-[2] '>
-            <div className='bg-gradient-to-r rounded p-[1px] from-[#7928ca] to-[#00bfff]'>
+            <div className='bg-gradient-to-r  rounded p-[1px] from-[#7928ca] to-[#00bfff]'>
             <button
               className='text-2xl button-primary-y'
               onClick={() => setIsOpen(!isOpen)}
